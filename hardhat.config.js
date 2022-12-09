64423b5db462fd2e96438a053f8f75bd70d88f34
slazengerx0x <80111336+slazengerx0x@users.noreply.github.com>
2022-12-09 19:02:59 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: '0.8.17',
   networks: {
     goerli: {
-      url: "https://virulent-still-dew.ethereum-goerli.discover.quiknode.pro/5d892eabc92820526559fafdbc455292da6dc280/",
+      url: process.env.STAGING_QUICKNODE_KEY,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
   },
